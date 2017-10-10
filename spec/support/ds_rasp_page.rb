f6e4b9017b84e84a87a2ee44ba1a171f820d6d8c
Anton Karpov <an.karpov@cti.ru>
2017-10-10 12:32:52 +0300

    простейщий сценарий готов вместе с нужными элементами

--- spec/support/ds_rasp_page.rb
+++ spec/support/ds_rasp_page.rb
@@ -4,6 +4,9 @@ class DSRasp_page < SitePrism::Page
   element :stantion_from, :xpath, "//app-autocomplete-station[contains(., 'Станция отправления')]//input"
   element :stantion_to, :xpath, "//app-autocomplete-station[contains(., 'Станция прибытия')]//input"
   element :but_find, 'button.margin-bottom.k-button.k-primary', text: "Поиск"
+  elements :table_rows, 'tr[data-kendo-grid-item-index]'
+  element :table_elem_moscow_belgorod, 'td', text: "МОСКВА КУР — БЕЛГОРОД"
+  elements :all_table, "td"
 
 
 end
@@ -22,6 +25,16 @@ module DSRasp_module
     @dsrasp.stantion_to.send_keys st_select
     sleep 1
     @dsrasp.stantion_to.send_keys :enter
+  end
+
+  def date_verifier(time_to_verify = "now")
+    if time_to_verify = "now"
+      t = Time.now
+      table_date_format = t.strftime("%d.%m.%Y")
+      puts "Поезда показаны в текущую дату".green if find(:xpath, "//td[contains(., '#{table_date_format}')]")
+    else
+      puts "Поезда показаны на '#{time_to_verify}'".green if find(:xpath, "//td[contains(., '#{time_to_verify}')]")
+    end
   end
 
 end

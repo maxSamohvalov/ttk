cae112ded70e1dcf4f91dab2c3204a3eade4d4eb
MaxWork <m.samokhvalov@cti.ru>
2017-10-11 15:07:58 +0300

    sdf

--- spec/steps/ds-inv.rb
+++ spec/steps/ds-inv.rb
@@ -2,24 +2,11 @@ module DSInv
 include CTI_panel_module
 include DSRasp_module
 
-  step "открывается окно портала" do
-    @page = CTI_panel.new
-    @page.load
-    expect(@page.first_tab_name == "ДС – Расписание")
-  end
-
-  step 'мы находимся в справке :curr_page' do |curr_page|
-    page_check(curr_page)
-  end
 
   step 'вводит номер поезда :num' do |num|
     train_to_select(num)
   end
 
-  step 'нажимает кнопку Поиск' do
-    @dsrasp = DSRasp_page.new
-    @dsrasp.find.inv.click
-  end
 
   step 'получает наличие вагонов для инвалидов' do
     @dsrasp = DSRasp_page.new

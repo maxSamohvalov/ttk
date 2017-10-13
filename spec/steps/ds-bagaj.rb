e39c3c1752a2d5ad9cccd8d6fe353b199fe49a19
MaxWork <m.samokhvalov@cti.ru>
2017-10-13 17:41:49 +0300

    Незначительные правкиНезначительные правкиНезначительные правки

--- spec/steps/ds-bagaj.rb
+++ spec/steps/ds-bagaj.rb
@@ -8,21 +8,21 @@ include DSRasp_module
     @dsrasp = DSRasp_page.new
     @dsrasp.bag_selector.click
     @dsrasp.popup_ruch_kladi.click
-    puts "Выбрана ручная кладь"
+    puts "Выбрана ручная кладь".green
 
   end
 
   step 'выбирает Количество мест' do
     @dsrasp = DSRasp_page.new
     @dsrasp.table_bag_places.send_keys '1'
-    puts "Выбрано 1 место"
+    puts "Выбрано 1 место".green
 
   end
 
   step 'выбирает Вес' do
     @dsrasp = DSRasp_page.new
     @dsrasp.table_bag_ves.send_keys '100'
-    puts "Выбран вес 100 кг"
+    puts "Выбран вес 100 кг".green
 
   end
 
@@ -30,12 +30,12 @@ include DSRasp_module
     @dsrasp = DSRasp_page.new
     @dsrasp.tipeof_bag_selector.click
     @dsrasp.popup_vid_rascheta.click
-    puts "Выбран вид расчета Наличные"
+    puts "Выбран вид расчета Наличные".green
   end
 
   step 'получает расчет по стоимости багажа' do
     @dsrasp = DSRasp_page.new
-    sleep 2
+    sleep 3
     #puts @dsrasp.bag_price.value
     puts "Цена указана".green if expect(find("strong").text.length).to eq(18)
 
